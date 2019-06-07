cd220395b6928dde3c7b61f4aeb0d5ac0c33da30
MoyRodriguez <31139923+MoyRodriguez@users.noreply.github.com>
2019-06-07 16:53:30 -0500

    Add files via upload

---
+++ Practica5.sql
@@ -0,0 +1,341 @@
+
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('1', 'Aguascalientes', 'AGU', '001');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('2', 'Baja California', 'BCN', '002');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('3', 'Baja California Sur', 'BCS', '003');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('4', 'Campeche', 'CAM', '004');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('5', 'Chiapas', 'CHP', '005');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('6', 'Chihuahua', 'CHH', '006');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('7', 'Ciudad de México', 'CDMX', '007');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('8', 'Coahuila', 'COA', '008');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('9', 'Colima', 'COL', '009');
+
+INSERT INTO `Estado` (`Estado_id`, `Nombre_Est`, `Abreviatura_Est`, `Lada`) 
+VALUES ('10', 'Durango', 'DUR', '010');
+
+
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('1', 'San Marcos', 'SNM', '1');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('2', 'Mexicali', 'Mex', '2');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('3', 'La Paz', 'LPZ', '3');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('4', 'San Francisco de Campeche', 'SFC', '4');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('5', 'Tuxtla Gutiérrez', 'TXG', '5');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('6', 'Chihuahua', 'CHH', '6');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('7', 'Ciudad de México', 'CDMX', '7');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('8', 'Saltillo', 'SLT', '8');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('9', 'Colima', 'COL', '9');
+
+INSERT INTO `Ciudad` (`Ciudad_id`, `Nombre_Ciu`, `Abreviatura_Ciu`, `Estado_Ciu`) 
+VALUES ('10', 'Victoria de Durango', 'DUR', '10');
+
+
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('11', 'Plaza Cid', 'Av. Juarez 232', '80', '1', '2', '4i313124', '31');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('22', 'Center', 'Av_Adlfo_Lopez_1050', '222', '2', '3', '4234234', '324');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('33', 'Center', 'Av_Adlfo_Lopez_1050', '222', '2', '3', '4234234', '324');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('44', 'Zocalo', 'Zona Centro', '234', '7', '7', '4534235', '511');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('55', 'Roma', 'Col Roma', '252', '7', '7', '23523434', '511');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('100', 'Palacio', 'SeGob', '32', '7', '7', '3422', '511');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('66', 'Trost', 'Muralla', '451', '4', '6', '125435', '231');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('77', 'Mirador', 'Col Mirador', '34', '8', '2', '4232323', '342');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('88', 'Independencia', 'Col Fundadores', '423', '5', '10', '342352', '323');
+
+INSERT INTO `Taquilla` (`Taquilla_id`, `Nombre_Taq`, `Direccion_Taq`, `Gerente_Taq`, `Ciudad_Taq`, `Estado_Taq`, `Num_Tel`, `Lada`) 
+VALUES ('99', 'Celaya', 'Plaza Galerias', '52', '9', '9', '1231254', '231');
+
+
+
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('1', 'Efetivo', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('2', 'Tarjeta Debito', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('3', 'Tarjeta Credito', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('4', 'oxxo', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('5', 'Seven', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('6', 'Transferencia', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('7', 'Internet', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('8', 'Banco', 'Inmediato');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('9', 'Bitcoin', '3 dias');
+
+INSERT INTO `Forma_Pago` (`F_Pago_id`, `Nombre_Pag`, `Especificacion`) 
+VALUES ('10', 'Spei', '3 dias');
+
+
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('1', 'Concierto');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('2', 'Palenque');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('3', 'Show Comedia');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('4', 'Teatro');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('5', 'Presentacion');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('6', 'Stan-Up');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('7', 'Baile');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('8', 'Festival');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('9', 'Graducacion');
+
+INSERT INTO `Tipo_Evento` (`T_Evento_id`, `Nombre_Tev`) 
+VALUES ('10', 'otros');
+
+
+
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('314', 'Vip', '1', '1', '50', 'Disponible', '5550');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('123', 'Prefetente', '4', '4', '100', 'Disponible', '3200');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('342', 'Numerado', '7', '5', '300', 'Disponible', '2200');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('154', 'Gen Pref', '6', '9', '354', 'Disponible', '1200');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('1050', 'Ctad', '5', '6', '356', 'Disponible', '1000');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('1024', 'Esquina', '7', '9', '23', 'Disponible', '800');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('5000', 'bbees', '2', '5', '235', 'Disponible', '600');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('234', 'rthe', '5', '3', '232', 'Disponible', '400');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('3452', 'gee5g', '52', '34', '23', 'Disponible', '300');
+
+INSERT INTO `Tipo_Asento` (`T_Asiento_id`, `Area_Asiento`, `Seccion_Asiento`, `Fila_Asiento`, `Num_Asiento`, `Estatus_id`, `Precio_Asiento`) 
+VALUES ('4252', 'gd2', '1', '13', '10', 'Disponible', '250');
+
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('1', 'Arena Mty', 'Fundidra', '5000'), 
+('2', 'Arena Cdmx', 'centr', '314'), 
+('3', 'bellas artes', 'centr', '123'), 
+('4', 'Banamex', 'adlf mate', '1050'), 
+('5', 'plaza toros', 'jshqsugd', '342'),
+ ('6', 'ewea', 'eerg', '314'),
+ ('7', 'artes', 'mayo 232', '1024'), 
+ ('8', 'sdfsd', 'fdw', '3452'), 
+ ('9', 'fwefef', 'wefwef', '5000'),
+ ('10', 'fwefw', 'fwefwef', '314');
+ 
+ INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('400', 'Auditorio Citibanamex', '700', '3534');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('400', 'Auditorio Citibanamex', '700', '3463');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('401', 'Auditorio Pabellon M', '701', '3463');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('403', 'Cintermex', '703','4534');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('404', 'Parque Fundidora', '704', '34534');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('405', 'Teatro de la Ciudad San Nicolas', '705', '345');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('406', 'Teatro Municipal Sara Garcia', '706', '345');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('407', 'Teatro de la Ciudad ', '707', '345');
+
+INSERT INTO `Local_Evento` (`Local_Evento_id`, `Nombre_LocEve`, `Direccion_LocEve`, `Asientos`) 
+VALUES ('408','Domo Care','708', '3454');
+
+ 
+ 
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('1', 'Juan Gabriel', '1', '2019-06-27', '2');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('2', 'Sebastian yactura', '2', '2019-08-16', '4');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('3', 'Franc', '6', '2019-08-30', '1');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('4', 'Tec', '9', '2019-09-05', '2');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('5', 'John miltn', '10', '2019-10-24', '4');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('6', 'Tigres del norte', '7', '2019-10-31', '5');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('7', 'fgsg', '10', '2019-11-01', '10');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('8', 'tegdfg', '10', '2019-11-02', '9');
+
+INSERT INTO `Evento` (`Evento_id`, `Nombre_Eve`, `Tipo_Evento`, `Fecha_Evento`, `Local_Eve`) 
+VALUES ('9', 'ttgvv', '5', '2019-11-21', '8');
+
+
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) 
+VALUES(300,'Mariana','Rodriguez','Gonzalez');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES 
+(301,'Barbara Viridiana','Espinosa','Lara');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+302,'Gerardo Mauricio','Tello','Escobar');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+303,'Ivan','Carrillo','Aguilar');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+304,'Alejandro','Saldaña','Cabañas');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+305,'Monica','Lopez','Castillo');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+306,'Adan','Zuñiga','Garza');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+307,'Diego','Vitela','Herrera');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+308,'Pancracio','Garza','Garza');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+309,'Alejandro','Sifuentes','Rios');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+310,'Judith Alejandra','Candelaria','Sanchez');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+311,'Daniela Ivon','Delgado','Avalos');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+312,'Aneth Mariane','Mendez','Gonzalez');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES(
+313,'Jesus Angel','Garza','Treviño');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+314,'Andrea Anahi','Herbert','Perez');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+315,'Javier','Rios','Rios');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+316,'Pamela','Vera','Lopez');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+317,'Jaime Humberto','Garcia','Gaytan');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+318,'Andrea','Flores','Flores');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+319,'Ana Ofelia','Lugo','Contreras');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+320,'Evelin Citlali Anes','Castro','Perez');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+321,'Sara Daniela','Elizondo','Guajardo');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+322,'Jesus Heriberto','Gomez','Mercado');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+323,'Steve','Jhanson','Martins');
+
+INSERT INTO `Empleados` (`id_empleado`, `nombreemp`, `apellidoPemp`, `apellidoMemp`) VALUES (
+324,'Mario Alberto','Charles','Martínez');
+
