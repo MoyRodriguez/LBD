e4bd0d6ca42b95a4a19b78a71275d2fa657ef3bc
MoyRodriguez <31139923+MoyRodriguez@users.noreply.github.com>
2019-06-07 16:54:15 -0500

    Add files via upload

---
+++ Practica6.sql
@@ -0,0 +1,26 @@
+
+
+SELECT (Empleado.id_empleado), (Empleado.nombreemp), (Local_Evento.Local_Evento_id), (Local_Evento.Nombre_LocEve), (Local_Evento.Direccion_LocEve) FROM Empleado JOIN Local_Evento;
+
+
+
+
+SELECT * FROM Empleados WHERE id_empleado = '300';
+
+SELECT * FROM Empleados WHERE id_empleado = '301';
+
+SELECT * FROM Empleados WHERE id_empleado = '302';
+
+SELECT * FROM Local_Evento_id WHERE Direccion_LocEve = 'mayo 232';
+
+SELECT * FROM Local_Evento_id WHERE Direccion_LocEve = 'Fundidra';
+
+SELECT * FROM Local_Evento_id WHERE Direccion_LocEve= 'adlf mate';
+
+SELECT * FROM Local_Evento_id WHERE Direccion_LocEve = 'centr';
+
+SELECT * FROM Local_Evento_id WHERE Direccion_LocEve = 'wefwef';
+
+SELECT * FROM Forma_Pago WHERE F_Pago_id = '2';
+
+SELECT * FROM Evento WHERE Evento_id = '10';
